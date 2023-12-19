f5481d67d493b941a7c8159bced943f2c667a7ab
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-12-19 15:25:16 +0330

    edit model in app flights

--- flight/models.py
+++ flight/models.py
@@ -9,7 +9,8 @@ class AirLine(models.Model):
     allowed_cargo = models.PositiveSmallIntegerField(_('بار مجاز'))
     airport_cabin = models.CharField(_('کابین'), max_length=50)
     airport_class = models.CharField(_('کلاس نرخی'), max_length=50)
-    price = models.ForeignKey('Price', on_delete=models.PROTECT, related_name='airlines_price')
+    airport_number_of_seates = models.PositiveIntegerField(_('تعداد صندلی های هواپمیا'))
+    startus_airline = models.BooleanField(_("وضعیت شرکت هواپیمایی"), default=True)
 
     class Meta:
         db_table = 'airline'
@@ -17,13 +18,26 @@ class AirLine(models.Model):
         verbose_name_plural = 'AirLines'
 
 
-class AirLineAttribute(models.Model):
-    airline = models.ForeignKey(AirLine, on_delete=models.PROTECT, related_name='airline_attribute')
+class Flight(models.Model):
+    aairline = models.ForeignKey(AirLine, on_delete=models.PROTECT,related_name = 'flights', verbose_name='هواپمیا')
+    origin = models.CharField(_('مبدا'), max_length=50)
+    destination = models.CharField(_('مقصد'), max_length=50)
+    flight_start_time = models.DateTimeField(_('ساعت حرکت'))
+    flight_end_time = models.DateTimeField(_('ساعت فرود'))
+    status_flight = models.BooleanField(_("وضعیت پرواز"))
+
+    class Meta:
+        db_table = "flight"
+        verbose_name = 'Flight'
+        verbose_name_plural = 'Flights'
+
+
+class FlightAttribute(models.Model):
+    flight = models.ForeignKey(Flight, on_delete=models.PROTECT, related_name='flight_attribute')
     attribute = models.CharField(_('ويژگی'), max_length=50)
-    value_char = models.CharField(_('مقدار'), max_length=50)
+    value_char = models.CharField(_('  مقدار متنی کوتاه'), max_length=50)
     value_text = models.CharField(_("مقدار متنی"), max_length=50)
     value_datetime = models.DateTimeField(_('مقدار زمانی'))
-    
     
     class Meta:
         db_table = 'airline attribute'
@@ -32,12 +46,12 @@ class AirLineAttribute(models.Model):
 
 
 class Price(models.Model):
+    flight = models.ForeignKey(Flight, on_delete=models.PROTECT, related_name='prices')
     basic_price = models.DecimalField(_("قیمت پایه"), max_digits=15, decimal_places=3)
     taxes_and_duties = models.DecimalField(_("مالیات و عوارض"), max_digits=15, decimal_places=3)
     taxes_and_services = models.DecimalField(_("مالیات و خدمات"), max_digits=15, decimal_places=3)
     value_added_tax = models.DecimalField(_("مالیات بر ارزش افزوده"), max_digits=15, decimal_places=3)
     discount = models.DecimalField(_('تخفیف'), max_digits=15, decimal_places=5)
-    
     
     class Meta:
         db_table = 'price'
